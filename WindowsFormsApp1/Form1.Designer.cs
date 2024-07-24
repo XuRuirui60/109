bb4f3e6c1a0804f806922508ab1cb446f822ab2e
Ruirui60 <a10551106@gmail.com>
2024-07-24 15:04:51 +0800

    加入專案檔案。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace _109
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.close = new System.Windows.Forms.Button();
+            this.open = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.write = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.alloff = new System.Windows.Forms.Button();
+            this.allon = new System.Windows.Forms.Button();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.exit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(230, 15);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(391, 57);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Current Time:tt hh:mm:ss";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightGreen;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.close);
+            this.groupBox1.Controls.Add(this.open);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(365, 154);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Device Connect";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(166, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Device Offline";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Device Status:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(267, 46);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(82, 32);
+            this.close.TabIndex = 2;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(179, 46);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(82, 32);
+            this.open.TabIndex = 1;
+            this.open.Text = "Open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 32);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.LightBlue;
+            this.groupBox2.Controls.Add(this.write);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(465, 100);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(386, 154);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Device Password";
+            // 
+            // write
+            // 
+            this.write.Location = new System.Drawing.Point(235, 53);
+            this.write.Name = "write";
+            this.write.Size = new System.Drawing.Size(113, 61);
+            this.write.TabIndex = 5;
+            this.write.Text = "Write EEPROM";
+            this.write.UseVisualStyleBackColor = true;
+            this.write.Click += new System.EventHandler(this.write_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(41, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 36);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.LemonChiffon;
+            this.groupBox3.Controls.Add(this.alloff);
+            this.groupBox3.Controls.Add(this.allon);
+            this.groupBox3.Controls.Add(this.checkBox8);
+            this.groupBox3.Controls.Add(this.checkBox7);
+            this.groupBox3.Controls.Add(this.checkBox6);
+            this.groupBox3.Controls.Add(this.checkBox5);
+            this.groupBox3.Controls.Add(this.checkBox4);
+            this.groupBox3.Controls.Add(this.checkBox3);
+            this.groupBox3.Controls.Add(this.checkBox2);
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 302);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(839, 162);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "LED Control";
+            // 
+            // alloff
+            // 
+            this.alloff.Location = new System.Drawing.Point(453, 83);
+            this.alloff.Name = "alloff";
+            this.alloff.Size = new System.Drawing.Size(179, 62);
+            this.alloff.TabIndex = 9;
+            this.alloff.Text = "All Lights OFF";
+            this.alloff.UseVisualStyleBackColor = true;
+            this.alloff.Click += new System.EventHandler(this.alloff_Click);
+            // 
+            // allon
+            // 
+            this.allon.Location = new System.Drawing.Point(148, 83);
+            this.allon.Name = "allon";
+            this.allon.Size = new System.Drawing.Size(179, 62);
+            this.allon.TabIndex = 8;
+            this.allon.Text = "All Lights ON";
+            this.allon.UseVisualStyleBackColor = true;
+            this.allon.Click += new System.EventHandler(this.allon_Click);
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(688, 49);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(84, 28);
+            this.checkBox8.TabIndex = 7;
+            this.checkBox8.Text = "LED0";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            this.checkBox8.CheckedChanged += new System.EventHandler(this.checkBox8_CheckedChanged);
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(598, 49);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(84, 28);
+            this.checkBox7.TabIndex = 6;
+            this.checkBox7.Text = "LED1";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            this.checkBox7.CheckedChanged += new System.EventHandler(this.checkBox7_CheckedChanged);
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(508, 49);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(84, 28);
+            this.checkBox6.TabIndex = 5;
+            this.checkBox6.Text = "LED2";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            this.checkBox6.CheckedChanged += new System.EventHandler(this.checkBox6_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(418, 49);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(84, 28);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "LED3";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(328, 49);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(84, 28);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "LED4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(238, 49);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(84, 28);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "LED5";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(148, 49);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(84, 28);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "LED6";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(58, 49);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 28);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "LED7";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // exit
+            // 
+            this.exit.Location = new System.Drawing.Point(355, 470);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(159, 39);
+            this.exit.TabIndex = 10;
+            this.exit.Text = "EXIT";
+            this.exit.UseVisualStyleBackColor = true;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(870, 517);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("新細明體", 18F);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "109 學年度 工業類科學生技藝競賽 電腦修護職種 第二站 崗位號碼: 01";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button close;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button write;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button alloff;
+        private System.Windows.Forms.Button allon;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button exit;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace _109
+{
+    public partial class Form1 : Form
+    {
+        SerialPort BT = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+            comBoxSet();//加入偵測到的port口到combox裡面
+           
+            groupBox2.Enabled = false;
+            groupBox3.Enabled = false;
+            label2.BackColor = Color.Red;
+            label2.Text = "Device Offline";
+        }
+        public void comBoxSet()//加入偵測到的port口到combox裡面
+        {
+            comboBox1.Items.Clear();
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            comBoxSet();
+            textBox1.Text = $"Current Time:{DateTime.Now.ToString("tt hh:mm:ss")}";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (BT.IsOpen)
+            {
+                //串口已經處於打開狀態
+                BT.Close();    //關閉串口
+                groupBox2.Enabled = false;
+                groupBox3.Enabled = false;
+                label2.BackColor = Color.Red;
+                label2.Text = "Device Offline";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(comboBox1.Text.Trim()))
+            {
+                BT.PortName = comboBox1.Text;
+                BT.BaudRate = 38400;//藍芽預設連接速度
+                BT.WriteTimeout = 3000;//預設連接時間3秒
+                BT.Open();//開啟藍芽Tx com port
+                label2.BackColor = Color.Green;
+                label2.Text = "Device Online";
+                groupBox2.Enabled = true;
+                groupBox3.Enabled = true;
+            }
+        }
+
+        private void write_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length != 4)//只能輸入4個字
+            {
+                textBox2.Text = "";
+            }
+            else//否則清除
+            {
+                BT.Write("w" + textBox2.Text);
+            }
+        }
+
+        private void allon_Click(object sender, EventArgs e)
+        {
+            BT.Write("a1");
+            checkBox1.Checked = true;
+            checkBox2.Checked = true;
+            checkBox3.Checked = true;
+            checkBox4.Checked = true;
+            checkBox5.Checked = true;
+            checkBox6.Checked = true;
+            checkBox7.Checked = true;
+            checkBox8.Checked = true;
+        }
+
+        private void alloff_Click(object sender, EventArgs e)
+        {
+            BT.Write("a2");
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+            checkBox5.Checked = false;
+            checkBox6.Checked = false;
+            checkBox7.Checked = false;
+            checkBox8.Checked = false;
+        }
+
+
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox8_CheckedChanged(object sender, EventArgs e)
+        {
+            BT.Write("b0");
+        }
+
+        private void checkBox7_CheckedChanged(object sender, EventArgs e)
+        {
+            BT.Write("b1");
+        }
+
+        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        {
+            BT.Write("b2");
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            BT.Write("b3");
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            BT.Write("b4");
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            BT.Write("b5");
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            BT.Write("b6");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            BT.Write("b7");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((int)e.KeyChar < 48 | (int)e.KeyChar > 57)//限制只能輸入數字
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
